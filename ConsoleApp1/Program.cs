bdb400979d36e0dd15291221c4cd3b697b63743a
Roua Smisem <rouasmisem02@gmail.com>
2024-06-03 11:57:54 +0300

    new branch

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, changed!");
             Console.WriteLine("Hello, test by Roua!");
+            Console.WriteLine("Hello, test adding new branch");
 
 
         }
